1eda4641234bb2a9c28124c07b2301320cfa9f33
youzenghe <2442616509@qq.com>
2026-02-09 13:24:40 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -37,4 +37,11 @@ public interface EmployeeMapper {
      * @return
      */
     void update(Employee  employee);
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
