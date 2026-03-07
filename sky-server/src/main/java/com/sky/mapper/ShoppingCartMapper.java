70a24dc982f2fdd816ccd94de236915c566a6635
youzenghe <2442616509@qq.com>
2026-03-07 17:36:26 +0800

    第九天业务代码(绕过退款)

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -24,4 +24,11 @@ public interface ShoppingCartMapper {
     void deleteByUserId(Long userId);
     @Delete("delete from shopping_cart where id = #{id}")
     void deleteById(Long id);
+
+    /**
+     * 批量插入购物车数据
+     *
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
