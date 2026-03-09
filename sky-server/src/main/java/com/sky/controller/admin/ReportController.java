ce2fa1d674258e6672a7c9764817966b0414b844
youzenghe <2442616509@qq.com>
2026-03-09 17:50:48 +0800

    图形报表业务代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -0,0 +1,63 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
+import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/admin/report")
+@Api(tags = "数据统计相关接口")
+@Slf4j
+public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+    @GetMapping("/turnoverStatistics")
+    @ApiOperation("营业额统计")
+    public Result<TurnoverReportVO> turnoverStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("营业额统计：{}到{}", begin, end);
+        return Result.success(reportService.getTurnoverStatistics(begin, end));
+    }
+
+    @GetMapping("/userStatistics")
+    @ApiOperation("用户统计相关接口")
+    public Result<UserReportVO> userStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("用户统计：{}到{}", begin, end);
+        return Result.success(reportService.getUserStatistics(begin, end));
+    }
+
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计接口")
+    public Result<OrderReportVO> ordersStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("订单统计：{}到{}", begin, end);
+        return Result.success(reportService.getOrderStatistics(begin, end));
+    }
+
+    @GetMapping("/top10")
+    @ApiOperation("销量排名")
+    public Result<SalesTop10ReportVO> getTop10(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("销量排名：{}到{}", begin, end);
+        return Result.success(reportService.getSalesTop10(begin, end));
+    }
+}
